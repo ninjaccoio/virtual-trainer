1a137a526995df3616975a2cf26ad9a075081004
ninjaccoio <saviniloris@gmail.com>
2022-05-02 19:13:40 +0200

    setting up firebase hosting and github autodeploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HO DEPLOYATO DA GITHUB. MIAO
+          HO DEPLOYATO DA GITHUB. MIAO BAO BAO
         </p>
       </header>
     </div>
