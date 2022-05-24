39b474a190a63362414bf630a813a3451871f5b8
ninjaccoio <saviniloris@gmail.com>
2022-05-24 18:14:24 +0200

    register page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -4,9 +4,9 @@ import IPageProps from '../../interfaces/page.interface';
 import {auth} from '../../config/firebase';
 import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 import logging from '../../config/logging';
-import {Paper, Avatar, TextField, Button, FormControlLabel, FormGroup, Stack, InputAdornment, Typography} from '@mui/material';
+import {Box, Paper, Avatar, TextField, Button, FormControlLabel, FormGroup, Stack, InputAdornment, Typography} from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MailOutlinedIcon from '@mui/icons-material/MailOutline';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import GoogleIcon from '@mui/icons-material/Google';
 
@@ -27,61 +27,67 @@ const LoginPage: React.FC<IPageProps> = (props) => {
                 setAuthing(false);
             });
     };
-    const paperStyle = {padding: 20, heigth: 'auto', width: '100%', maxwidth: '200px', margin: '20px auto'};
+    const paperStyle = {padding: 20, heigth: 'auto', width: '85%', margin: '20px auto'};
     const avatarStyle = {backgroundColor: 'green'};
     return (
         <>
             <Paper elevation={5} style={paperStyle}>
-                <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
-                    <Avatar style={avatarStyle}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <h2>Login</h2>
+                <form>
+                    <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+                        <Avatar style={avatarStyle}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <h2>Login</h2>
 
-                    <TextField
-                        label="Email"
-                        variant="outlined"
-                        placeholder="E-Mail"
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <AccountCircleIcon />
-                                </InputAdornment>
-                            )
-                        }}
-                    />
-                    <TextField
-                        label="Password"
-                        variant="outlined"
-                        type="password"
-                        placeholder="Password"
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <LockOutlinedIcon />
-                                </InputAdornment>
-                            )
-                        }}
-                    />
+                        <TextField
+                            label="Email"
+                            variant="outlined"
+                            placeholder="E-Mail"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <MailOutlinedIcon />
+                                    </InputAdornment>
+                                )
+                            }}
+                            fullWidth
+                        />
+                        <TextField
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            placeholder="Password"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <LockOutlinedIcon />
+                                    </InputAdornment>
+                                )
+                            }}
+                            fullWidth
+                        />
 
-                    <FormGroup>
-                        <FormControlLabel control={<Checkbox />} label="Ricordami" />
-                    </FormGroup>
-                    <Button type="submit" variant="contained" fullWidth>
-                        Login
-                    </Button>
-                    <Button variant="contained" style={{backgroundColor: 'red'}} startIcon={<GoogleIcon />} onClick={() => signInWithGoogle()} disabled={authing} fullWidth>
-                        Sign in with Google
-                    </Button>
-                    <Typography>
-                        <Link to="#">password dimenticata?</Link>
-                    </Typography>
-                    <Typography>
-                        {' '}
-                        non hai un account?
-                        <Link to="/register">clicca qui</Link>
-                    </Typography>
-                </Stack>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} label="Ricordami" />
+                        </FormGroup>
+                        <Button type="submit" variant="contained" fullWidth>
+                            Login
+                        </Button>
+
+                        <Button variant="contained" style={{backgroundColor: 'red'}} startIcon={<GoogleIcon />} onClick={() => signInWithGoogle()} disabled={authing} fullWidth>
+                            Accedi con Google
+                        </Button>
+                        <Typography>
+                            <Link to="#">password dimenticata?</Link>
+                        </Typography>
+
+                        <Typography>
+                            {' '}
+                            non hai un account?
+                            <Link to="/register">crea un account</Link>
+                        </Typography>
+                    </Stack>
+                </form>
             </Paper>
         </>
     );
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import IPageProps from '../../interfaces/page.interface';
-import {Button} from '@mui/material';
+import {Paper, Avatar, TextField, Button, FormControlLabel, FormGroup, Stack, InputAdornment, Typography} from '@mui/material';
+import Checkbox from '@mui/material/Checkbox';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import MailOutlinedIcon from '@mui/icons-material/MailOutline';
 
 const RegisterPage: React.FC<IPageProps> = (props) => {
     const navigate = useNavigate();
@@ -9,10 +13,73 @@ const RegisterPage: React.FC<IPageProps> = (props) => {
         navigate(-1);
     };
 
+    const paperStyle = {padding: 20, heigth: 'auto', width: '85%', margin: '20px auto'};
+    const avatarStyle = {backgroundColor: 'red'};
     return (
         <>
-            <h1>Register Page</h1>
-            <Button onClick={goBack}> INDIETRO </Button>
+            <Paper elevation={5} style={paperStyle}>
+                <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+                    <Avatar style={avatarStyle}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <h2>Register</h2>
+
+                    <TextField
+                        label="Username"
+                        variant="outlined"
+                        placeholder="Username"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <AccountCircleOutlinedIcon />
+                                </InputAdornment>
+                            )
+                        }}
+                        fullWidth
+                        required
+                    />
+
+                    <TextField
+                        label="Email"
+                        variant="outlined"
+                        placeholder="E-Mail"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <MailOutlinedIcon />
+                                </InputAdornment>
+                            )
+                        }}
+                        fullWidth
+                        required
+                    />
+                    <TextField
+                        label="Password"
+                        variant="outlined"
+                        type="password"
+                        placeholder="Password"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockOutlinedIcon />
+                                </InputAdornment>
+                            )
+                        }}
+                        fullWidth
+                        required
+                    />
+
+                    <Button type="submit" variant="contained" fullWidth>
+                        Registrati
+                    </Button>
+
+                    <Typography>
+                        {' '}
+                        hai gia' un account?
+                        <Link to="/login">accedi</Link>
+                    </Typography>
+                </Stack>
+            </Paper>
         </>
     );
 };
