771fbebe6ee03543262d07d7cb3b101d2d098aa9
ninjaccoio <saviniloris@gmail.com>
2022-06-01 15:43:15 +0200

    html fix for mobile

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Application from './Application';
+import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
